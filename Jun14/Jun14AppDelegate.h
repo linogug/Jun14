ee54cfe7cd895cfa3d8b2a767382178af983fc0c
Lino Guglielmo <linogug@aol.com>
2012-06-13 20:52:28 -0400

    Initial Commit

---
+++ Jun14/Jun14AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun14AppDelegate.h
+//  Jun14
+//
+//  Created by Lino Guglielmo on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun14AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jun14/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jun14
+//
+//  Created by Lino Guglielmo on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Jun14AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jun14AppDelegate class]));
+	}
+}
